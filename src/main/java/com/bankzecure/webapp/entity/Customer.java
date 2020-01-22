a4f6a197fd9196ff72937825fc9d3cef4c7854b2
Benoît Hubert <benoithubert@gmail.com>
2020-01-22 06:16:40 +0100

    CreditCard entity/controller/repository, vulnerability to SQLi

---
+++ src/main/java/com/bankzecure/webapp/entity/Customer.java
@@ -0,0 +1,40 @@
+package com.bankzecure.webapp.entity;
+
+import java.util.Date;
+
+public class Customer {
+
+  private long id;
+  private String identifier;
+  private String firstName;
+  private String lastName;
+  private String email;
+
+  public Customer(long id, String identifier, String firstName, String lastName, String email) {
+    this.id = id;
+    this.identifier = identifier;
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.email = email;
+  }
+
+  public long getId() {
+    return id;
+  }
+
+  public String getIdentifier() {
+    return identifier;
+  }
+
+  public String getFirstName() {
+    return firstName;
+  }
+
+  public String getLastName() {
+    return lastName;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+}
